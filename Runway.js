71dd321459cce647790e141f03a6415b4f8022a5
Gurdev Gill <108082625+Gurdev135@users.noreply.github.com>
2023-02-15 12:00:14 +0000

    Update Runway.js

--- Runway.js
+++ Runway.js
@@ -10,9 +10,22 @@ class Runway {
     
     add(plane) {
 
-        if (Runway.planes.length>Runway.MAX_PLANES_ALLOWED_ON_ALL_RUNWAYS) {
+        if (Runway.planes.length>=Runway.MAX_PLANES_ALLOWED_ON_ALL_RUNWAYS) {
             throw new Error ("runways at full capacity!")
         }
         Runway.planes.push(plane)
     }
+
+    remove(plane){
+        if(Runway.planes.length<1){
+            throw new Error('runway is empty!')
+        }
+
+        const index = Runway.planes.indexOf(plane);
+        if(index>-1){
+            Runway.planes.splice(index, 1);
+        }
+    }
 }
+
+module.exports = Runway
